e9c9a8b66d4cd5942b3ae9302e5f160371d0d4c7
Mulki Anaz aliza <57121849+anazmuu@users.noreply.github.com>
2022-11-29 20:22:25 +0700

    Update 4_Number.py

--- 1. Dasar Python/4_Number.py
+++ 1. Dasar Python/4_Number.py
@@ -5,16 +5,34 @@ number_b = 10
 number_c = -5
 
 print(number_a + number_b) #operasi pertambahan
+#output : 16
 print(number_a - number_b) #operasi pengurangan
+#output : -3
 print(number_a * number_b) #operasi perkalian
+#output : 60
 print(number_a / number_b) #operasi pembagian
+#output : 0.6
 print(number_a % number_b) #operasi modulus
+#output : 6
 print(str(number_a) + " adalah angka 6") #int di str kan terlebih dahulu
+#output :6 adalah angka 6
 print(abs(number_c)) #abs merubah negatif menjadi positif
+#output :5
 print(pow(5, 3)) #pow adalah pemangkatan 5 * 5 * 5
+#output : 125.0
 print(max(12, 15, 4, 20)) #mencari nilai maksimum
+#output : 20
 print(min(12, 15, 4, 20)) #mencari nilai minimum
+#output : 4
 print(round(3.6)) #mencari hasil nilai bulat
+#output : 4
 print(floor(3.7))
+#output : 3
 print(ceil(3.6))
-print(sqrt(64)) #pembulatan
+#output : 4
+
+des_a = 5.5
+des_b = 8.3
+
+print(des_a + des_b)
+#output : 13.8
