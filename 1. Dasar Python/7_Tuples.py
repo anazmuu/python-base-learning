a585c6dd3a28452c3a516c34ea9e31fde4f5236b
Mulki Anaz aliza <57121849+anazmuu@users.noreply.github.com>
2022-12-03 14:42:00 +0700

    Update 7_Tuples.py

--- 1. Dasar Python/7_Tuples.py
+++ 1. Dasar Python/7_Tuples.py
@@ -7,8 +7,14 @@ hewan = ("Ayam", "Kucing") #Data tidak bisa di rubah
 angka = (2, 4, 6, 8, 10)
 
 print(hewan)
+#output : ('Ayam', 'Kucing')
+print(angka)
+#output : (2, 4, 6, 8, 10)
 print(hewan[1]) #pemanggilan secara elemen
-
+#output : Kucing
 print(len(hewan)) #length tuple
+#output : 2
 print(max(angka))
+#output : 10
 print(min(angka))
+#output : 2
