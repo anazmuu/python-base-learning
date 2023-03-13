9beef6febb7bdd1aeeee80134d97034c57f7eecc
Mulki Anaz aliza <57121849+anazmuu@users.noreply.github.com>
2023-03-13 20:22:58 +0700

    Update 6_For.py

--- 2. Fundamental Python/6_For.py
+++ 2. Fundamental Python/6_For.py
@@ -4,6 +4,11 @@ Guru = ["Mulki", "Boni", "Risma"]
 for Guru in Guru:
     print(Guru)
 
+##Output :##
+Mulki
+Boni
+Risma 
+      
 #String
 print("\n")
 for i in "Mulki":
@@ -11,12 +16,39 @@ for i in "Mulki":
 
 print("\n")
 
+##Output :##
+M
+u
+l
+k
+i
+
 for index in range(3, 10):
     print(index)
 
-
+##Output :##
+3
+4
+5
+6
+7
+8
+9
+    
+    
 for ind in range(5):
     if ind == 0:
         print("First Iteration")
     else:
         print("Bukan Pertama")
+
+##Output :##
+First Iteration
+Bukan Pertama
+Bukan Pertama
+Bukan Pertama
+Bukan Pertama
+
+
+
+
